bf27c2a299bc1b49f117f39c5c7be2c0a69fd0b7
Maksim Torburg <torburg@yandex.ru>
2019-01-07 17:30:05 +0300

    post ckecking in entities_add.php

--- src/php/entities_add.php
+++ src/php/entities_add.php
@@ -3,52 +3,50 @@ include($_SERVER['DOCUMENT_ROOT'] . "/header.php");
 use \Amo\Api\AmoApi;
 //проверка авторизации?..
 
-if ($_POST) {
-    if ($_POST['count'] >= 0 && $_POST['count'] <= 10000) {
+if ($_POST['count'] >= 0 && $_POST['count'] <= 10000) {
 
-        $count = $_POST['count'];
+    $count = $_POST['count'];
 
-        $login = 'mfilippov@team.amocrm.com';
-        $hash = '46dc105cf215a952995d55378d858bedf78fd024';
+    $login = 'mfilippov@team.amocrm.com';
+    $hash = '46dc105cf215a952995d55378d858bedf78fd024';
 
-        $amoApi = new AmoApi();
-        $amoApi->authorization($login, $hash);
+    $amoApi = new AmoApi();
+    $amoApi->authorization($login, $hash);
 
-        $entities = [
-            'contacts' => [],
-            'companies' => [],
-            'leads' => [],
-            'customers' => []
-        ];
+    $entities = [
+        'contacts' => [],
+        'companies' => [],
+        'leads' => [],
+        'customers' => []
+    ];
 
-        do {
-            $n = 0;
-            //250 - recommended entities amount to add with one request
-            if ($count <= 250) {
-                $n = $count;
-            } else {
-                $n = 250;
-            }
-            $contacts = $amoApi->collect('contact', $n, $entities);
-            $response = $amoApi->add('contacts', $contacts);
-            $response = $response["_embedded"]['items'];
-            $entities['contacts'] = $amoApi->response_processing($response);
+    do {
+        $n = 0;
+        //250 - recommended entities amount to add with one request
+        if ($count <= 250) {
+            $n = $count;
+        } else {
+            $n = 250;
+        }
+        $contacts = $amoApi->collect('contact', $n, $entities);
+        $response = $amoApi->add('contacts', $contacts);
+        $response = $response["_embedded"]['items'];
+        $entities['contacts'] = $amoApi->response_processing($response);
 
-            $companies = $amoApi->collect('company', $n, $entities);
-            $response = $amoApi->add('companies', $companies);
-            $response = $response["_embedded"]['items'];
-            $entities['companies'] = $amoApi->response_processing($response);
+        $companies = $amoApi->collect('company', $n, $entities);
+        $response = $amoApi->add('companies', $companies);
+        $response = $response["_embedded"]['items'];
+        $entities['companies'] = $amoApi->response_processing($response);
 
-            //customers and leads don't connect -> response_processing doesn't required
-            $customers = $amoApi->collect('customers', $n, $entities);
-            $response = $amoApi->add('customers', $customers);
+        //customers and leads don't connect -> response_processing doesn't required
+        $customers = $amoApi->collect('customers', $n, $entities);
+        $response = $amoApi->add('customers', $customers);
 
-            $leads = $amoApi->collect('leads', $n, $entities);
-            $amoApi->add('leads', $leads);
+        $leads = $amoApi->collect('leads', $n, $entities);
+        $amoApi->add('leads', $leads);
 
-            $count -= $n;
-        } while ($count > 0);
-    }
+        $count -= $n;
+    } while ($count > 0);
 
     $fields = $amoApi->collect('fields', 1);
     $request = $amoApi->add('fields', $fields);
@@ -66,7 +64,7 @@ if ($_POST) {
         "синий",
         "зелёный"
     ];
-    #@TODo need to get all contacts (1 response return max 500 contacts)
+#@TODo need to get all contacts (1 response return max 500 contacts)
     $response = $amoApi->get('contacts');
     $response = $response['_embedded']['items'];
     $contacts = [];
@@ -87,3 +85,4 @@ if ($_POST) {
     }
     $response = $amoApi->update('contacts', $contacts);
 }
+
